d1b01b3306df01b0b03d8bb8931a34ed817d53f7
alpaka99 <ruvan123@naver.com>
2022-11-15 01:40:28 +0900

    [221114] Fetch User Location & Map annotations

--- Miro_TastyRoad/Miro_TastyRoad/MapView.swift
+++ Miro_TastyRoad/Miro_TastyRoad/MapView.swift
@@ -11,12 +11,35 @@ import MapKit
 struct MapView: View {
     @EnvironmentObject var places: Places
     
-    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
+    // start location: Seoul, Jongak Station
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.570212883541835, longitude: 126.98303503392553), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
     @State private var searchText = ""
+    @State private var userLocation = MKUserLocation()
+    
+    let locationFetcher = LocationFetcher()
+    
     var body: some View {
         NavigationView {
             ZStack {
-                Map(coordinateRegion: $mapRegion)
+                Map(coordinateRegion: $mapRegion, showsUserLocation: true, annotationItems: places.placeList) { place in
+                    MapAnnotation(coordinate: place.coordinate) {
+                        ZStack {
+                            Image(systemName: "flag.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .foregroundColor(.red)
+                                .frame(width: 20, height: 20)
+                            
+//                            Circle()
+//                                .stroke(.red)
+//                                .frame(width:25, height: 25)
+                        }
+                        .onTapGesture {
+                            print(place.name)
+                        }
+                        
+                    }
+                }
                     .ignoresSafeArea()
                 
                 Circle()
@@ -28,13 +51,29 @@ struct MapView: View {
             .toolbar {
                 ToolbarItem(placement: .bottomBar) {
                     Button("SavePlace") {
-                        places.saveNewPlace(name: "TestRow", description: "TestDescription")
+                        places.saveNewPlace(name: "TestRow", description: "TestDescription", latitude: mapRegion.center.latitude, longitude: mapRegion.center.longitude)
                         
-                        print(places.placeList)
+//                        print(places.placeList)
                     }
+                }
+                
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Move to user's location") {
+                            self.locationFetcher.start()
+                            if let location = self.locationFetcher.lastKnownLocation {
+                                print("Your location is \(location)")
+                                mapRegion.center = location
+                            } else {
+                                mapRegion.center = CLLocationCoordinate2D(latitude: 37.570212883541835, longitude: 126.98303503392553)
+                            }
+                                }
                 }
             }
         }
+    }
+    
+    func mark() {
+        
     }
 }
 
--- Miro_TastyRoad/Miro_TastyRoad/Place.swift
+++ Miro_TastyRoad/Miro_TastyRoad/Place.swift
@@ -6,17 +6,28 @@
 //
 
 import Foundation
+import CoreLocation
 
-struct Place: Hashable {
+struct Place: Identifiable, Hashable {
+    static func == (lhs: Place, rhs: Place) -> Bool {
+        lhs.name < rhs.name
+    }
+    
+    let id = UUID()
     let name: String
     let description: String
+    let latitude: Double
+    let longitude: Double
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
 }
 
 class Places: ObservableObject {
     var placeList = [Place]()
     
-    func saveNewPlace(name: String, description: String) {
-        var newPlace = Place(name: name, description: description)
+    func saveNewPlace(name: String, description: String, latitude: Double, longitude: Double) {
+        var newPlace = Place(name: name, description: description, latitude: latitude, longitude: longitude)
         objectWillChange.send()
         placeList.append(newPlace)
         
